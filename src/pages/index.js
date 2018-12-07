dfdb60653dc9de7d2e43bd513e3420f369e1fcee
Zain Fathoni <zain.fathoni@gmail.com>
2018-12-07 17:01:28 +0800

    Use Typography and add 3 pages manually

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,11 @@
-import React from "react"
+import React from 'react'
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div>
+    <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+    <p>
+      What do I like to do? Lots of course but definitely enjoy building
+      websites.
+    </p>
+  </div>
+)
