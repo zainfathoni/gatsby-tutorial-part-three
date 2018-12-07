518f495498d5a27f49a02e51d4bd4e506fdb2794
Zain Fathoni <zain.fathoni@gmail.com>
2018-12-07 17:03:34 +0800

    Add Layout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import Layout from '../components/layout'
 
 export default () => (
-  <div>
+  <Layout>
     <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
     <p>
       What do I like to do? Lots of course but definitely enjoy building
       websites.
     </p>
-  </div>
+  </Layout>
 )
